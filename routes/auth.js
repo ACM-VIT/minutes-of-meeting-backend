8e468ea52342d38b61c5a8bec501d93c865b1d43
ANISH0309 <mittalanish789@gmail.com>
2021-05-08 00:53:46 +0530

    add client URL to .env
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -6,29 +6,29 @@ const router = express.Router();
 // @route   GET /auth/google
 
 router.get(
-	"/google",
-	passport.authenticate("google", { scope: ["profile", "email"] })
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 // @desc    goolge Auth callback
 // @route   GET /auth/goolge/callback
 
 router.get(
-	"/google/callback",
-	passport.authenticate("google", {
-		failureRedirect: "http://localhost:3000/autherror",
-	}),
-	(req, res) => {
-		res.redirect("http://localhost:3000");
-	}
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: process.env.CLIENT_ERROR_URL,
+  }),
+  (req, res) => {
+    res.redirect(process.env.CLIENT_URL);
+  }
 );
 
 // @desc    Logout User
 // route    /auth/logout
 
 router.get("/logout", (req, res) => {
-	req.logout();
-	res.redirect("http://localhost:3000");
+  req.logout();
+  res.redirect(process.env.CLIENT_URL);
 });
 
 module.exports = router;

7259f837efed792d89684f4e2ed1d531e3d17da4
Likhit Ajeesh <likhitajeesh20@gmail.com>
2021-08-03 22:54:44 +0400

    fix: error handling

--- middleware/jwt_helper.js
+++ middleware/jwt_helper.js
@@ -1,39 +1,36 @@
 const jwt = require("jsonwebtoken");
-const createError = require("http-errors");
 
 module.exports = {
   // eslint-disable-next-line consistent-return
   verifyToken: (req, res, next) => {
-    // Check whether authorization header present
-    if (!req.headers.authorization)
-      return next(createError.Unauthorized(401, "No Bearer token sent"));
-
     const authHeader = req.headers.authorization;
+
+    // Check whether authorization header present
+    if (!authHeader) {
+      return res.status(401).send("No Bearer token sent");
+    }
     const bearerToken = authHeader.split(" ");
     const bearer = bearerToken[0];
     const token = bearerToken[1];
+
+    // Check whether Bearer starts with "B"
+    if (bearer !== "Bearer") {
+      return res.status(401).send("Token not authorized with Bearer");
+    }
 
     // Verification
     // eslint-disable-next-line consistent-return
     jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
       if (err) {
-        // Check whether Bearer starts with "B"
-        if (bearer !== "Bearer") {
-          return res.status(401).send("Bearer token not sent");
-        }
-
         if (err.name === "JsonWebTokenError") {
-          return next(res.status(401).send("Invalid Token"));
-        }
-
-        if (err.name === "Bearer null") {
-          return next(res.status(401).send("Invalid Token sent"));
+          return res.status(401).send("Invalid token sent");
         }
       }
       req.payload = payload;
 
       // User id
-      console.log(req.payload.id);
+      const userId = req.payload.id;
+      console.log({ "userId after token verification": userId });
       next();
     });
   },
